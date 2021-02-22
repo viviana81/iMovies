4ed75a4692f0e187c3a06e325a09c46cf1e2ccd7
Viviana <vivianacapolvenere@gmail.com>
2021-02-22 08:45:27 +0100

    setup project

---
+++ iMovies/Scene/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iMovies
+//
+//  Created by Viviana Capolvenere on 22/02/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
